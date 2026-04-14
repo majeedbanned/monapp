4a7048cb879739b94d470dfb56f1c772ec79a4b5
majid <majeed.ghasemi@gmail.com>
2026-04-14 12:35:18 +0400

    fff

--- site-config.js
+++ site-config.js
@@ -2,7 +2,7 @@ window.MONELY_SITE = Object.freeze({
   siteName: "Monely",
   siteUrl: "https://monelyapp.com",
   socialImageUrl: "https://monelyapp.com/social-preview.png",
-  appStoreUrl: "",
+  appStoreUrl: "https://apps.apple.com/us/app/monely/id6759529990",
   ga4MeasurementId: "G-Q8DZ22RE57",
   googleSiteVerification: "",
   emailCaptureProvider: "mailchimp",
